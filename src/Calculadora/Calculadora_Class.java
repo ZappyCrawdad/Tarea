6581b214b48336659b74c6b5d46d5c970a00c314
Zappy <zappy660@gmail.com>
2022-02-27 16:49:20 -0600

    Inicial commit

---
+++ src/Calculadora/Calculadora_Class.java
@@ -0,0 +1,180 @@
+/*
+██████╗░██╗░░░██╗██╗  ███████╗██╗░░░░░██╗  ████████╗░█████╗░██╗░░░██╗░█████╗░██████╗░
+██╔══██╗╚██╗░██╔╝╚═╝  ██╔════╝██║░░░░░██║  ╚══██╔══╝██╔══██╗██║░░░██║██╔══██╗██╔══██╗
+██████╦╝░╚████╔╝░░░░  █████╗░░██║░░░░░██║  ░░░██║░░░██║░░██║╚██╗░██╔╝███████║██████╔╝
+██╔══██╗░░╚██╔╝░░░░░  ██╔══╝░░██║░░░░░██║  ░░░██║░░░██║░░██║░╚████╔╝░██╔══██║██╔══██╗
+██████╦╝░░░██║░░░██╗  ███████╗███████╗██║  ░░░██║░░░╚█████╔╝░░╚██╔╝░░██║░░██║██║░░██║
+╚═════╝░░░░╚═╝░░░╚═╝  ╚══════╝╚══════╝╚═╝  ░░░╚═╝░░░░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝
+*/
+
+package Calculadora;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class Calculadora_Class extends JFrame{
+   
+       private JButton [] botones = new JButton[15];
+       
+       JTextField caja1, caja2;
+       JTextField [] fields = {caja1, caja2};
+       int index = 0;
+       
+       private class NumberActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            if(e.getSource()==botones[0]){
+                caja1.setText(caja1.getText() + "0");
+                if(e.getSource() == botones[0]){
+                fields[index].setText(fields[index].getText() + "0");
+}
+                //System.out.println("0");
+            }
+            if(e.getSource()==botones[1]){
+                caja1.setText(caja1.getText() + "1");
+                //System.out.println("1");
+            }
+            if(e.getSource()==botones[2]){
+                caja1.setText(caja1.getText() + "2");
+               // System.out.println("2");
+            }
+            if(e.getSource()==botones[3]){
+                caja1.setText(caja1.getText() + "3");
+                //System.out.println("3");
+            }
+            if(e.getSource()==botones[4]){
+                caja1.setText(caja1.getText() + "4");
+                //System.out.println("4");
+            }
+            if(e.getSource()==botones[5]){
+                caja1.setText(caja1.getText() + "5");
+                //System.out.println("5");
+            }
+            if(e.getSource()==botones[6]){
+                caja1.setText(caja1.getText() + "6");
+                //System.out.println("6");
+            }
+            if(e.getSource()==botones[7]){
+                caja1.setText(caja1.getText() + "7");
+                //System.out.println("7");
+            }
+            if(e.getSource()==botones[8]){
+                caja1.setText(caja1.getText() + "8");
+                //System.out.println("8");
+            }
+            if(e.getSource()==botones[9]){
+                caja1.setText(caja1.getText() + "9");
+                //System.out.println("9");
+            }
+            if(e.getSource()==botones[10]){
+                JTextField [] fields = {caja1, caja2};
+                int index = 0;
+                fields[index].setText(fields[index].getText() + "0");
+            }
+            if(e.getSource()==botones[11]){
+                JTextField [] fields = {caja1, caja2};
+                int index = 0;
+                fields[index].setText(fields[index].getText() + "0");
+            }
+            if(e.getSource()==botones[12]){
+                JTextField [] fields = {caja1, caja2};
+                int index = 0;
+                fields[index].setText(fields[index].getText() + "0");
+            }
+            if(e.getSource()==botones[13]){
+                JTextField [] fields = {caja1, caja2};
+                int index = 0;
+            }
+        }
+    }
+       
+    public Calculadora_Class(){
+       
+        setTitle("Calculadora Simple");
+        this.setSize(520, 500);
+        this.setLocationRelativeTo(null);
+        this.setIconImage(new ImageIcon(getClass().getResource("icono.png")).getImage());
+        iniciar();
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    private void iniciar(){
+        
+        NumberActionListener buttonListener = new NumberActionListener();
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        this.getContentPane().add(panel);
+        
+        JPanel panel2 = new JPanel();
+        panel2.setBounds(15, 10, 250, 400);
+        panel2.setLayout(new GridLayout(5, 3, 2, 2));
+        panel2.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED), "Numeros", TitledBorder.CENTER,TitledBorder.DEFAULT_POSITION));
+        panel.add(panel2);
+        
+        JPanel panel3 = new JPanel();
+        panel3.setBounds(300, 25, 200, 100);
+        panel3.setLayout(new GridLayout(2, 2, 2, 20));
+        panel3.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED), "Variables", TitledBorder.CENTER,TitledBorder.DEFAULT_POSITION));
+        panel.add(panel3);
+        
+        JLabel etiqueta1 = new JLabel("a");
+        panel3.add(etiqueta1);
+        
+        caja1 = new JTextField();
+        caja1.setEditable(false);
+        panel3.add(caja1);
+        
+        JLabel etiqueta2 = new JLabel("b");
+        panel3.add(etiqueta2);
+
+        JTextField caja2 = new JTextField();
+        caja2.setEditable(false);
+        panel3.add(caja2);
+        
+        JLabel etiqueta3 = new JLabel("Operación");
+        etiqueta3.setBounds(370, 120, 130, 30);
+        panel.add(etiqueta3);
+        
+        JTextField caja3 = new JTextField();
+        caja3.setBounds(300, 150, 197, 30);
+        caja3.setEditable(false);
+        panel.add(caja3);
+        
+        JLabel etiqueta4 = new JLabel("R =");
+        etiqueta4.setBounds(140, 420, 200, 30);
+        panel.add(etiqueta4);
+        
+        JTextField caja4 = new JTextField();
+        caja4.setBounds(160, 420, 200, 30);
+        caja4.setEditable(false);
+        panel.add(caja4);
+        
+        String[]tags = {"S", "R", "P", "D", "."};
+        
+        int j = 0;
+       for(int i = 0; i < botones.length; i++){
+           if(i<=9){
+               botones[i] = new JButton(String.valueOf(i));
+               botones[i].addActionListener(buttonListener);
+               panel2.add(botones[i]);
+           }
+           else{
+               botones[i] = new JButton(tags[j]);
+               panel2.add(botones[i]);
+               j++;
+           }
+       }
+       
+    }
+}
---
+++ src/Calculadora/Calculadora_Main.java
@@ -0,0 +1,13 @@
+
+package Calculadora;
+
+public class Calculadora_Main {
+
+    public static void main(String[] args) {
+        
+        Calculadora_Class calcu = new Calculadora_Class();
+        calcu.setResizable(false);
+        calcu.setVisible(true);
+    }
+    
+}
